e1012da47ee80b6699b5ecb83519bd3d3e576063
Bebrik-blin <lovehueta@gmail.com>
2025-01-10 16:08:06 +0300

    Add files via upload

---
+++ ESPmqttV0.ino
@@ -0,0 +1,15 @@
+//Внимание! Билд тестовый блядь, ничё не трогать! Всё на соплях!КОСТЯ НЕ БОМБИ, ЗДЕСЬ ВСЁ ХУЙНЯ И НЕ ОПТИМИЗИРОВАНО, Я ЗНАЮ!
+
+#include <ESP8266WiFi.h>
+
+char ssid[] = "ssid"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
