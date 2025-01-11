c46407674206d0fc549077cab937c0d573e800d8
Bebrik-blin <lovehueta@gmail.com>
2025-01-11 10:05:13 +0300

    Update ESPmqttV0.ino

--- ESPmqttV0.ino
+++ ESPmqttV0.ino
@@ -1,57 +1,34 @@
-//Внимание! Билд тестовый блядь, ничё не трогать! Всё на соплях!КОСТЯ НЕ БОМБИ, ЗДЕСЬ ВСЁ ХУЙНЯ И НЕ ОПТИМИЗИРОВАНО, Я ЗНАЮ!
+//Билд тестовый, мозги не ебать ничего не трогат(всё на замёрших соплях, изоленте и жвачке)
 
-//#include "ESP8266WiFi.h"
-//#include "uMQTTBroker.h"
+#include <ESP8266WiFi.h>
+#include "uMQTTBroker.h"
 #include "confige.h"
 
-char ssid[] = "ssid"; //id in network
-char password = "12321"; //password from wifi
+uMQTTBroker myBroker;
 
-
-
-class MyMqtt
+void WifiClient()
 {
-public:
-  virtual void ClientConect(IPAddress addr, String client_id) {
-      Serial.println(addr.toString()+" ("+client_id+") connected");
-  }
-  virtual void ClientDisconect(IPAddress addr, String client_id) {
-      Serial.println(addr.toString()+" ("+client_id+") disconnected");
-  }
-}
-
-myMqtt myBroker;
-
-
-void startWiFiClient()
-{
-  Serial.println("Connecting to "+(String)ssid);
-  WiFi.mode(WIFI_STA);
+  Serial.println("Connection as client..");
   WiFi.begin(ssid, pass);
   
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  
-  Serial.println("WiFi connected");
-  Serial.println("IP address: " + WiFi.localIP().toString());
+  Serial.println("WiFi connected.");
+  Serial.println("IP: " + WiFi.localIP().toString());
 }
 
-
-void startWiFiAP()
+void WiFiAP()
 {
-  WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, pass);
   Serial.println("AP started");
   Serial.println("IP address: " + WiFi.softAPIP().toString());
 }
 
-void setup() {
-  if(wifiAP){startWIFIAP();}
-  else{startWIFIClient();}
 
+void setup() {
+  if(wifiAP) WiFiAP();
+  else WifiClient();
+
+  Serial.println("Starting MQTT broker");
+  myBroker.init();
 }
 
 void loop() {
